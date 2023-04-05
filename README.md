39486d62e118bd628c3fec8e4a0f531a07e28540
Peter Dedene <peter@zenjoy.be>
2023-04-05 16:28:51 +0200

    Add Chocolatey package for talosctl

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chocolatey-Packages
+Some packages that can be installed via chocolatey.org
+
+My Chocolatey profile.  
+https://chocolatey.org/profiles/dedene
+
+Chocolatey - like yum or apt-get, but for Windows  
+More information about chocolatey is here.  
+https://github.com/chocolatey/choco
---
+++ talosctl/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64 = 'https://github.com/siderolabs/talos/releases/download/v1.3.6/talosctl-windows-amd64.exe'
+$checksum64 = 'dbefbeb7190e5d3178bf1605dabbcdf4ad7b31ee9975aed212a53080c9b3041b8db21cba415664e1d85321ad27e3ef10b1ae4839621b51aade82805382b862c7'
+$checksumType64 = 'sha512'
+$File = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'talosctl.exe'
+$file = Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName `
+ -FileFullPath $File `
+ -Url64bit $url64  `
+ -CheckSum64 $checksum64 `
+ -CheckSumType64 $checksumType64
