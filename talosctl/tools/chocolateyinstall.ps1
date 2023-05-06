d8967413d6b575faa1a885a52c45b1e3a4340bc1
Peter Dedene <peter@zenjoy.be>
2023-05-06 10:47:16 +0200

    Update to 1.4.1

--- talosctl/tools/chocolateyinstall.ps1
+++ talosctl/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64 = 'https://github.com/siderolabs/talos/releases/download/v1.4.0/talosctl-windows-amd64.exe'
-$checksum64 = 'fe71eb9f32f3fbab515b71d2d10c36e4b6246e2100bcf639b66b2df5629167549d5ab3ff68ff57d2990dfc16d5f9fce6a233f525582291f4fa08bf90a671d1e5'
+$url64 = 'https://github.com/siderolabs/talos/releases/download/v1.4.1/talosctl-windows-amd64.exe'
+$checksum64 = 'f35b0109380173e5171561f5e5b855728a8f7b1966db07dcfa02c4762035142e9e7554aa8f9759ac236b7770d50d106d0eaaf63052eb6bd6b8d16be19189c817'
 $checksumType64 = 'sha512'
 $File = Join-Path (Join-Path $env:ChocolateyInstall (Join-Path 'lib' $env:ChocolateyPackageName)) 'talosctl.exe'
 $file = Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName `
